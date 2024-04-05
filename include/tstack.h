d5b697f3b43d38799036f2ba4e16450024c0ed2e
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-04-05 13:51:44 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,9 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <string>
 template<typename T, int size>
 class TStack {
-
 private:
     T data[100];
     int top_index;
@@ -12,27 +12,27 @@ private:
 public:
     TStack() : top_index(-1) {}
     void push(T value) {
-        if (top_index == size - 1) {
+        if (top_index != size - 1) {
+          data[++top_index] = value;
+        } else {
             throw std::string("Stack is full");
-            return;
         }
-        data[++top_index] = value;
     }
 
     T pop() {
         if (isEmpty()) {
             throw std::string("Stack is empty");
-            return T();
+        } else {
+            return data[top_index--];
         }
-        return data[top_index--];
     }
 
     T top() {
         if (isEmpty()) {
             throw std::string("Stack is empty");
-            return T();
+        } else {
+            return data[top_index];
         }
-        return data[top_index];
     }
 
     bool isEmpty() {
