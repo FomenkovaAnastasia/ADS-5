6dec294ada11dafc958362e685a44a2f1540bd09
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-04-05 11:59:58 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,39 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[size];
+    int top_index;
+public:
+    TStack() : top_index(-1) {}
+
+    void push(T value) {
+        if (top_index == size - 1) {
+            std::cerr << "Stack is full" << std::endl;
+            return;
+        }
+        data[++top_index] = value;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty" << std::endl;
+            return T();
+        }
+        return data[top_index--];
+    }
+
+    T top() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty" << std::endl;
+            return T();
+        }
+        return data[top_index];
+    }
+
+    bool isEmpty() {
+        return top_index == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
