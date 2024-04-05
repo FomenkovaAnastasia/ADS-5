213bb5a3d175194ed303ece25c4516097ab33d3b
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-04-05 16:38:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include "tstack.h"
 #include <stack>
+#include "alg.h"
 
 bool isOperator(char c) {
   return (c == '+' || c == '-' || c == '*' || c == '/');
@@ -17,27 +18,35 @@ int precedence(char c) {
 }
 
 std::string infx2pstfx(std::string inf) {
-  std::string postfix;
-  TStack<char, 100> stack;
-  for (char c : inf) {
-    if (isdigit(c)) {
-      postfix = postfix + c + ' ';
-    } else if (c == '(') {
-      stack.push(c);
-    } else if (precedence(c)) {
-      while (!stack.isEmpty() && isOperator(stack.get()) >= isOperator(c)) {
-        postfix = postfix + stack.get() + ' ';
-        stack.pop();
-      }
-      stack.push(c);
-    } else if (c == ')') {
-      while (!stack.isEmpty() && stack.get() != '(') {
-        postfix = postfix + stack.get() + ' ';
-        stack.pop();
-      }
-      stack.pop();
+    std::string postfix;
+    TStack<char, 100> stack;
+    for (char c : inf) {
+        if (isdigit(c)) {
+            postfix = postfix + c + ' ';
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (precedence(c)) {
+            while (!stack.isEmpty() && isOperator(stack.get()) >= isOperator(c)) {
+                postfix = postfix + stack.get() + ' ';
+                stack.pop();
+            }
+            stack.push(c);
+        } else if (c == ')') {
+            while (!stack.isEmpty() && stack.get() != '(') {
+                postfix = postfix + stack.get() + ' ';
+                stack.pop();
+            }
+            stack.pop();
+        }
     }
-  }
+    while (!stack.isEmpty()) {
+        postfix = postfix + stack.get() + ' ';
+        stack.pop();
+    }
+    if (!postfix.empty()) {
+        postfix.pop_back();
+    }
+    return postfix;
 }
 
 int eval(std::string post) {
